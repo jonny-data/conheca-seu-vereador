d9dc58f6307f0d3a6dcc24281c992c4eade5483c
João Batista Neto <neto.joaobatista@gmail.com>
2012-05-25 12:15:21 -0300

    Adição de configuração de nível e exibição de erros

--- src/jonnydata/config.php
+++ src/jonnydata/config.php
@@ -5,6 +5,12 @@
  */
 namespace jonnydata;
 
+//configuração do ambiente. TRUE para produção, FALSE para desenvolvimento.
+$production = false;
+
+ini_set('display_errors', $production?'Off':'On');
+error_reporting($production?E_ALL & ~E_DEPRECATED & ~E_STRICT:E_ALL|E_STRICT);
+
 //configuração do include_path
 ini_set('include_path', implode(PATH_SEPARATOR, array_unique(
 	array_merge(
