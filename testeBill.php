2912c35419688df1e808f9884f679f5ddefa4636
João Batista Neto <neto.joaobatista@gmail.com>
2012-05-25 18:00:52 -0300

    Limpando repositório

--- testeBill.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'src/jonnydata/config.php';
-use jonnydata\csv\data\dal\mongo\MongoDBDataAccessFactory;
-$factory = new MongoDBDataAccessFactory();
-$billDataAccess = $factory->createBillDataAccess();
-var_dump($billDataAccess);
